911443519545f44ff5f722d0f5ccba4fc4284c2d
farhannrs <45318554+farhannrs@users.noreply.github.com>
2021-12-27 18:03:03 +0500

    firstCommit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FMA-Automation-Web
---
+++ cypress/integration/FMA/test.spec.js
@@ -0,0 +1,6 @@
+describe('Find the toast message', function() {
+   
+  it('https://www.OLX.com/', function() {
+     cy.visit('https://www.olx.com.pk/')
+    })
+ })
