c46755cb23c6b410aa8c376c085784661e7a384c
farhannrs <45318554+farhannrs@users.noreply.github.com>
2021-12-28 10:01:28 +0500

    newcommands

--- cypress/integration/FMA/test.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Find the toast message', function() {
-   
-  it('https://www.OLX.com/', function() {
-     cy.visit('https://www.olx.com.pk/')
-    })
- })
---
+++ cypress/integration/olx/Script.js
@@ -0,0 +1,21 @@
+describe('Find the toast message', function() {
+   let minimumAmount = 40000
+   let maximumAmount = 120000
+   
+  it('https://www.OLX.com/', function() {
+
+   
+     cy.visit('https://www.olx.com.pk/')
+     cy.get('img[class ="b5cfbf0b"]').click()
+     cy.get('a[class="_7d3f8c9a"]').contains('Mobile Phones').click().wait(2000)
+     cy.get('input[placeholder="Min"]').invoke('val', minimumAmount)
+     cy.get('input[placeholder="Max"]').invoke('val', maximumAmount)
+
+     
+     for(let i = 0 ; i<10 ;  i++){
+      cy.get('span[class="_7978e49c _2e82a662"]').eq(i)
+     }
+
+
+    })
+ })
